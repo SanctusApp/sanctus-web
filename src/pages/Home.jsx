bd2dfcd481fef90141a9b1739014f6586a3a1f91
Your Name <you@example.com>
2021-10-12 15:28:02 +0200

    deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,6 @@ import Main from "../components/home/Main";
 import AboutUs from "../components/home/AboutUs";
 import Features from "../components/home/Features";
 import ContactUs from "../components/home/ContactUs";
-import Store from '../components/home/Store';
-import Testimonials from '../components/Testimonials';
 //--------------------------------------------
 
 
